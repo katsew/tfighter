a17c29ae336ec7c3161489c2740f97c6b2ecd0fa
katsew <y.katsew@gmail.com>
2017-10-29 01:25:15 +0900

    Add hit detection for attack.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,44 +1,38 @@
 import "pixi.js"
 import * as KeyboardJS from "keyboardjs"
 
-const app = new PIXI.Application();
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 450;
+const app = new PIXI.Application(GAME_WIDTH, GAME_HEIGHT);
 app.renderer.backgroundColor = 0x000066;
-document.body.appendChild(app.view);
+
+const appElem = document.getElementById("app");
+appElem.appendChild(app.view);
 
 let assetPath = process.env.ASSET_PATH ? process.env.ASSET_PATH : "";
 
 const FixRot = Math.PI * 0.5;
-const HitRound = 50;
+const DeathZone = 50;
+const HitRange = 30;
 const PunchRange = 150;
+
 PIXI.loader
     .add("turtle", `${assetPath}/assets/img/kame.png`)
     .add("marimo", `${assetPath}/assets/img/mariball.png`)
-    .add("marimoBlue", `${assetPath}/assets/img/mariballBlue.png`)
-    .add("marimoYellow", `${assetPath}/assets/img/mariballYellow.png`)
-    .add("marimoPink", `${assetPath}/assets/img/mariballPink.png`)
     .add("punch", `${assetPath}/assets/img/punch.png`)
     .add("punchLong", `${assetPath}/assets/img/punchLong.png`)
     .load(function(loader, resources) {
-        const turtle = new PIXI.Sprite(resources.turtle.texture);
-        const marimo = new PIXI.Sprite(resources.marimo.texture);
-        const marimoBlue = new PIXI.Sprite(resources.marimoBlue.texture);
-        marimoBlue.anchor.x = 0.5
-        marimoBlue.anchor.y = 0.5
-        const marimoPink = new PIXI.Sprite(resources.marimoPink.texture);
-        marimoPink.anchor.x = 0.5
-        marimoPink.anchor.y = 0.5
-        const marimoYellow = new PIXI.Sprite(resources.marimoYellow.texture);
-        marimoYellow.anchor.x = 0.5
-        marimoYellow.anchor.y = 0.5
+
         const punch = new PIXI.Sprite(resources.punch.texture);
         punch.anchor.x = 0.5;
         punch.anchor.y = 0.75;
 
         const punchLong = new PIXI.Sprite(resources.punchLong.texture);
         punchLong.anchor.x = 0.5;
-        punchLong.anchor.y = 0.95;
+        punchLong.anchor.y = 0.93;
         punchLong.visible = false;
 
+        const turtle = new PIXI.Sprite(resources.turtle.texture);
         turtle.x = app.renderer.width * 0.85;
         turtle.y = app.renderer.height / 2;
         turtle.rotation = Math.PI * -0.5;
@@ -47,47 +41,21 @@ PIXI.loader
         turtle.addChild(punch);
         turtle.addChild(punchLong);
 
+        const marimo = new PIXI.Sprite(resources.marimo.texture);
         marimo.anchor.x = 0.5
         marimo.anchor.y = 0.5
+
         const poison = new PIXI.Graphics();
         poison.beginFill(0x8800FF);
-        poison.drawCircle(0, 0, HitRound);
+        poison.drawCircle(0, 0, DeathZone);
         poison.endFill();
         poison.x = (app.renderer.width / 4) * Math.random() + (poison.width / 2);
         poison.y = app.renderer.height * Math.random();
+        poison.addChild(marimo);
 
-        const poisonBlue = new PIXI.Graphics();
-        poisonBlue.beginFill(0x8800FF);
-        poisonBlue.drawCircle(0, 0, HitRound);
-        poisonBlue.endFill();
-        poisonBlue.x = (app.renderer.width / 4) * Math.random() + (poisonBlue.width / 2);
-        poisonBlue.y = app.renderer.height * Math.random();
-
-
-        const poisonPink = new PIXI.Graphics();
-        poisonPink.beginFill(0x8800FF);
-        poisonPink.drawCircle(0, 0, HitRound);
-        poisonPink.endFill();
-        poisonPink.x = (app.renderer.width / 4) * Math.random() + (poisonPink.width / 2);
-        poisonPink.y = app.renderer.height * Math.random();
-
-        const poisonYellow = new PIXI.Graphics();
-        poisonYellow.beginFill(0x8800FF);
-        poisonYellow.drawCircle(0, 0, HitRound);
-        poisonYellow.endFill();
-        poisonYellow.x = (app.renderer.width / 4) * Math.random() + (poisonYellow.width / 2);
-        poisonYellow.y = app.renderer.height * Math.random();
-
-        // poison.addChild(marimo);
-        // poisonBlue.addChild(marimoBlue);
-        // poisonPink.addChild(marimoPink);
-        poisonYellow.addChild(marimoYellow);
-        
-        // app.stage.addChild(poison, poisonBlue, poisonPink, poisonYellow);
-        app.stage.addChild(poisonYellow);
+        app.stage.addChild(poison);
         app.stage.addChild(turtle);
         
-
         KeyboardJS.bind("shift + left", function(e) {
             punch.rotation -= 0.05;
             punchLong.rotation -= 0.05;
@@ -102,11 +70,9 @@ PIXI.loader
             punch.visible = false;
             punchLong.visible = true;
             if (
-                // IsDestroy(marimo, punch) ||
-                // IsDestroy(marimoBlue, punch) ||
-                // IsDestroy(marimoPink, punch) ||
-                IsDestroy(marimoYellow, turtle, punch)
+                IsDestroy(marimo, turtle, punchLong)
             ) {
+                ShowWin();
                 console.log("Destroy!!!");
             }
             setTimeout(function() {
@@ -124,38 +90,51 @@ PIXI.loader
             turtle.rotation += 0.05;
         });
 
-        function IsHit(M, T) {
-            let diffX = T.position.x - M.position.x;
-            let diffY = T.position.y - M.position.y;
+        function IsDead(Poison, Turtle) {
+            let diffX = Turtle.position.x - Poison.position.x;
+            let diffY = Turtle.position.y - Poison.position.y;
             let diff = (diffX * diffX) + (diffY * diffY);
-            return (HitRound * HitRound) > diff;
+            return (DeathZone * DeathZone) > diff;
         }
 
-        function IsDestroy(M, K, P) {
-            let diffX = (Math.cos(P.rotation) * PunchRange);
-            let diffY = (Math.sin(P.rotation) * PunchRange);
-            let diff = (diffX * diffX) + (diffY * diffY);
+        // @todo Destroy if punch point walk through the moss ball point.
+        function IsDestroy(M, T, P) {
+            let OT = T.getGlobalPosition();
+            let OMGlobal = M.getGlobalPosition();
+            let OM = {
+                x: OMGlobal.x,
+                y: OMGlobal.y
+            };
 
-            console.log("MarimoPos: ", M.getGlobalPosition());
-            console.log("PunchPos: ", P.getGlobalPosition());
-            console.log("KamePos: ", K.getGlobalPosition());
+            let rot = P.rotation;
+            let originalRot = -Math.PI + P.rotation;
+            let OP = {
+                x: OT.x + Math.cos(originalRot) * PunchRange,
+                y: OT.y + Math.sin(originalRot) * PunchRange
+            };
+            console.log("OM: ", OM);
+            console.log("OP: ", OP);
 
-            let mRound  = M.width / 2;
-            console.log(mRound * mRound, diff);
-            return (mRound * mRound) > (diff - PunchRange);
+            let MP = {
+                x: OP.x - OM.x,
+                y: OP.y - OM.y
+            };
+            console.log("MP: ", MP)
+
+            let AbsMP = Math.sqrt( (MP.x * MP.x) + (MP.y * MP.y) );
+            console.log("Length MP: ", AbsMP);
+
+            return HitRange >= AbsMP;
         }
 
         KeyboardJS.bind("up", function(e) {
             turtle.position.x += 5 * Math.cos( (turtle.rotation - FixRot) );
             turtle.position.y += 5 * Math.sin( (turtle.rotation - FixRot) );
             if (
-                IsHit(poison, turtle) ||
-                IsHit(poisonPink, turtle) ||
-                IsHit(poisonBlue, turtle) ||
-                IsHit(poisonYellow, turtle)
+                IsDead(poison, turtle)
             ) {
                 console.log("Hit!!!");
-                ShowHit();
+                ShowGameOver();
             }
         });
         KeyboardJS.bind("down", function(e) {
@@ -163,18 +142,15 @@ PIXI.loader
             turtle.position.y -= 5 * Math.sin( (turtle.rotation - FixRot) );
 
             if (
-                // IsHit(poison, turtle) ||
-                // IsHit(poisonPink, turtle) ||
-                // IsHit(poisonBlue, turtle) ||
-                IsHit(poisonYellow, turtle)
+                IsDead(poison, turtle)
             ) {
-                ShowHit();
+                ShowGameOver();
             }
         });
-        
+
         let hasHit = false;
-        function ShowHit() {
-            const message = new PIXI.Text("Game Over", {
+        function ShowWin() {
+            const message = new PIXI.Text("You Win!", {
                 fontFamily: "Arial",
                 fontSize: 32,
                 fill: "white"
@@ -183,8 +159,24 @@ PIXI.loader
             message.position.y = (app.renderer.height / 2) - (message.height / 2);
             if (!hasHit) {
                 app.stage.addChild(message);
-                app.stage.removeChild(turtle);
+                app.stage.removeChild(poison);
             }
             hasHit = true;
+        }
+        
+        let hasDead = false;
+        function ShowGameOver() {
+            const message = new PIXI.Text("Game Over", {
+                fontFamily: "Arial",
+                fontSize: 32,
+                fill: "white"
+            });
+            message.position.x = (app.renderer.width / 2) - (message.width / 2);
+            message.position.y = (app.renderer.height / 2) - (message.height / 2);
+            if (!hasDead) {
+                app.stage.addChild(message);
+                app.stage.removeChild(turtle);
+            }
+            hasDead = true;
         }
     });
